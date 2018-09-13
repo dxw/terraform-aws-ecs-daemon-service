8f07842ee08658a76cae61fcd3e388c195ac4c10
Chris Wright <chris@dxw.com>
2018-09-13 15:33:20 +0100

    (Chore) Initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "aws_ecs_task_definition" "main" {
+  family                   = "${var.environment}-${var.service_name}"
+  container_definitions    = "${var.task_definition}"
+  task_role_arn            = "${var.task_role_arn}"
+  network_mode             = "bridge"
+  cpu                      = "${var.task_cpu}"
+  memory                   = "${var.task_memory}"
+  requires_compatibilities = ["EC2"]
+  execution_role_arn       = "${var.awsvpc_task_execution_role_arn}"
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "${var.service_name}"
+  iam_role        = "${var.ecs_service_role}"
+  cluster         = "${var.ecs_cluster_id}"
+  task_definition = "${aws_ecs_task_definition.main.arn}"
+
+  health_check_grace_period_seconds  = 30
+
+  load_balancer {
+    target_group_arn = "${var.alb_target_group_arn}"
+    container_name   = "${var.container_name}"
+    container_port   = "${var.container_port}"
+  }
+
+  scheduling_strategy = "DAEMON"
+}
---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "environment" {
+  description = "Logical name of the environment, will be used as prefix and in tags."
+  type        = "string"
+}
+
+variable "task_role_arn" {
+  description = "The AWS IAM role that will be provided to the task to perform AWS actions."
+  type        = "string"
+  default     = ""
+}
+
+variable "ecs_cluster_id" {
+  description = "The id of the ECS cluster"
+  type        = "string"
+}
+
+variable "service_name" {
+  description = "Logical name of the service."
+  type        = "string"
+}
+
+variable "lb_target_group_arn" {
+  description = "Load Balancer Target Group ARN"
+  type        = "string"
+}
+
+variable "task_definition" {
+  description = "The AWS task definition of the containers to be created."
+  type        = "string"
+}
+
+variable "ecs_service_role" {
+  default = ""
+}
+
+variable "task_cpu" {
+  description = "Task CPU"
+  type        = "string"
+  default     = ""
+}
+
+variable "task_memory" {
+  description = "Task Memory"
+  type        = "string"
+  default     = ""
+}
