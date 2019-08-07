628dab8392f518b59242d54006563a1774e2d30c
Chris Wright <chris@dxw.com>
2019-08-07 15:08:25 +0100

    (Chore) terraform v0.12 updates

--- main.tf
+++ main.tf
@@ -7,7 +7,27 @@ resource "aws_ecs_task_definition" "main" {
   memory                   = "${var.task_memory}"
   requires_compatibilities = ["EC2"]
   execution_role_arn       = "${var.task_execution_role_arn}"
-  volume                   = ["${var.task_volumes}"]
+
+  dynamic "volume" {
+    for_each = var.task_volumes
+
+    content {
+      name                        = volume.value.name
+      host_path                   = lookup(volume.value, "host_path", null) == null ? null : volume.value.host_path
+
+      dynamic "docker_volume_configuration" {
+        for_each = lookup(volume.value, "docker_volume_configuration", null) == null ? [] : volume.value.docker_volume_configuration
+
+        content {
+          scope         = lookup(docker_volume_configuration.value, "scope", null) == null ? null : docker_volume_configuration.value.scope
+          autoprovision = lookup(docker_volume_configuration.value, "autoprovision", null) == null ? null : docker_volume_configuration.value.autoprovision
+          driver        = lookup(docker_volume_configuration.value, "driver", null) == null ? null : docker_volume_configuration.value.driver
+          driver_opts   = lookup(docker_volume_configuration.value, "driver_opts", null) == null ? null : docker_volume_configuration.value.driver_opts
+          labels        = lookup(docker_volume_configuration.value, "labels", null) == null ? null : docker_volume_configuration.value.labels
+        }
+      }
+    }
+  }
 }
 
 # Service with bridge networking mode
